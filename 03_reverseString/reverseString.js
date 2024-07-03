9e6537581e6ee3f7129d874c823fb99016275200
Brandon Lin <brandonlin9716@gmail.com>
2024-07-03 16:48:45 -0700

    Complete exercise 03_reverseString
    First time using VS code JS debugger! Neat!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // go backwards and construct a string from char
+    let reversedString = "";
+    for (let i = string.length; i > 0; i--) {
+        let index = i - 1 ;
+        reversedString += string.charAt(index);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
