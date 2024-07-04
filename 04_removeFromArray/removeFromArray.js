bf59d8f76b8a846d5d9e49d4250a0ac2aca7a31a
Brandon Lin <brandonlin9716@gmail.com>
2024-07-03 17:21:59 -0700

    Complete exercise removeFromArray
    Solution uses .includes() method, for each, and anon fucntions.
    Very neat. I would like to try practicing those in my next solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, unwanted) {
+  for (let i = 1; i < arguments.length; i++) {
+    unwanted = arguments[i];
+    let unwantedIndex = array.indexOf(unwanted);
+    while (unwantedIndex > -1) {
+      array.splice(unwantedIndex, 1);
+      unwantedIndex = array.indexOf(unwanted, unwantedIndex);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
