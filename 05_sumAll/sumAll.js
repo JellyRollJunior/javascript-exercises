83258345cdaa1a58be7f054746ff52ac9bf4b5d7
Brandon Lin <brandonlin9716@gmail.com>
2024-07-03 17:34:52 -0700

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (sumOne, sumTwo) {
+  if (
+    sumOne < 0 ||
+    sumTwo < 0 ||
+    typeof sumOne !== "number" ||
+    typeof sumTwo !== "number"
+  ) {
+    return "ERROR";
+  }
+  let min = Math.min(sumOne, sumTwo);
+  let max = Math.max(sumOne, sumTwo);
+  let sum = 0;
+  for (min; min <= max; min++) {
+    sum += min;
+  }
+  return sum;
 };
 
 // Do not edit below this line
