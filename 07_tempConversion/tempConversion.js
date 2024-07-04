32e0e28cda0624eca4767ddc037fb755e2ce1294
Brandon Lin <brandonlin9716@gmail.com>
2024-07-03 18:11:23 -0700

    Complete exercise 07_tempConversion
    I learned how to round valeus to 1 decimal place today with JS. Neat!

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(FahrenheitTemp) {
+  let celsciusTemp = ((FahrenheitTemp - 32) * (5/9)).toFixed(1);
+  let roundedTemp = Math.round(celsciusTemp*10)/10;
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsciusTemp) {
+  let FahrenheitTemp = (celsciusTemp * (9/5) + 32).toFixed(1);
+  let roundedTemp = Math.round(FahrenheitTemp*10)/10;
+  return roundedTemp;
 };
 
 // Do not edit below this line
