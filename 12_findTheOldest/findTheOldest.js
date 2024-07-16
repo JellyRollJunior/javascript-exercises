978593f1e5032590c609e5f95e277a6b84419ae6
Brandon Lin <brandonlin9716@gmail.com>
2024-07-15 18:30:24 -0700

    complete exercise findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    // we can iterate thru the find them all (normal way)
+    // use reduce to return the person object
+    let oldestAge = 0;
+    let oldestPerson = people[0];
+    for (const person of people) {
+        let age = 0;
+        if (!person.yearOfDeath) {
+            let currentTime = new Date();
+            age = +currentTime.getFullYear() - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
 
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = person
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
