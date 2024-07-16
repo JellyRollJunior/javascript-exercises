fbe50586df3a1548c098ad7a14f7daf47bfbe47b
Brandon Lin <brandonlin9716@gmail.com>
2024-07-15 17:36:54 -0700

    Complete 09-palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().split("").filter((item) => {
+        return (item >= "a" && item <= "z") 
+        || (item >= "0" && item <= "9");
+    }).join("");
+    let j = string.length - 1;
+    for (let i = 0; i < string.length; i++) {
+        let left = string.charAt(i);
+        let right = string.charAt(j);
+        if (i >= j) break;
+        if (left != right) return false;
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
