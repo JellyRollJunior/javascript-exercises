0c298044137e9e5b2c9f03d5ae8a0dd624f644c2
Brandon Lin <brandonlin9716@gmail.com>
2024-07-15 17:01:15 -0700

    Complete exercise 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,49 @@
-const add = function() {
-	
+const add = function () {
+    return Array.from(arguments).reduce((sum, current) => {
+        return sum + current;
+    }, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function () {
+    return Array.from(arguments).reduce((difference, current) => {
+        return difference - current;
+    });
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+    return array.reduce((sum, current) => {
+        return sum + current;
+    }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+    return array.reduce((product, current) => {
+        return product * current;
+    }, 1);
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+    let returnValue = 1;
+    for (let i = 1; i <= power; i ++) {
+        returnValue *= base;
+    }    
+    return returnValue;
 };
 
-const factorial = function() {
-	
+const factorial = function (base) {
+    let returnValue = 1;
+    for (let i = 1; i <= base; i++) {
+        returnValue *= i;
+    }
+    return returnValue;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
