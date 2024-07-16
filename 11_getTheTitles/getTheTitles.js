290db83e997d0620b92c7e598f9363ad8fb550da
Brandon Lin <brandonlin9716@gmail.com>
2024-07-15 18:04:38 -0700

    Complete exercise 11-getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    // oops, just use map!
+    return books.reduce((titles, bookInfo) => {
+        titles.push(bookInfo.title);
+        return titles;
+    }, [])
 };
 
 // Do not edit below this line
