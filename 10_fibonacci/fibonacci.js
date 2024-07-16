14319701442b1ff66dc70f74d509d766cd3e34e6
Brandon Lin <brandonlin9716@gmail.com>
2024-07-15 17:50:33 -0700

    Complete 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = +n;
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
+    let fibonacciArray = [0, 1];
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result = fibonacciArray[0] + fibonacciArray[1];
+        fibonacciArray.shift();
+        fibonacciArray.push(result);
+    }
+    return result;
 };
 
 // Do not edit below this line
